6c559d28e381adfd6a7e453de5fd23c137843117
NotVeurects <123427154+NotVeurects@users.noreply.github.com>
2023-01-24 08:39:59 -0600

    Update Version.lua

--- Version.lua
+++ Version.lua
@@ -1,5 +1,5 @@
-if getgenv().Version == '2.3' then
-    getgenv().webhook = true
-else
+if getgenv().Version ~= '2.3' then
     getgenv().webhook = false
+elseif getgenv().Version == '2.3' then
+    getgenv().webhook = true
 end
